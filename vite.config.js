7ff57086cebdaadf266b4ed486cd7e875c9f56ac
Christoph Papczyk <christoph.papczyk@iti-software.de>
2023-05-01 19:48:10 +0200

    feat: add test coverage

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,28 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import svg from '@poppanator/sveltekit-svg';
+import istanbul from 'vite-plugin-istanbul';
+import { defineConfig } from 'vite';
 
-/** @type {import('vite').UserConfig} */
-const config = {
-	plugins: [sveltekit(), svg()]
-};
-
-export default config;
+export default defineConfig({
+	build: {
+		sourcemap: true
+	},
+	plugins: [
+		sveltekit(),
+		istanbul({
+			include: ['src/*'],
+			exclude: ['node_modules', 'test/'],
+			extension: ['.ts', '.svelte'],
+			all: true,
+			checkCoverage: true,
+			reporter: ['lcov'],
+			reportDir: './tests/coverage',
+			requireEnv: false,
+			forceBuildInstrument: true
+		}),
+		svg()
+	],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
+});
