312a4cc8f504abe0457d4464660b4330cee580be
Christoph Papczyk <christoph.papczyk@iti-software.de>
2023-05-02 17:19:46 +0200

    feat: adjust search tests

--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -1,7 +1,11 @@
 import { expect, test } from './baseFixtures.js';
 
 test.beforeEach(async ({ page }) => {
+	const productList = page.getByRole('list');
+
 	await page.goto('/');
+
+	await productList.waitFor();
 });
 
 test('filters products by name', async ({ page }) => {
@@ -21,10 +25,21 @@ test('shows all products when search filter is cleared', async ({ page }) => {
 	const searchTerm = 'Gouda';
 	const searchField = page.getByPlaceholder('Suche');
 
+	const productsCount = await page.getByRole('article').count();
+
 	await searchField.click();
 	await searchField.fill(searchTerm);
 
-	expect('name').toEqual('name');
+	let filteredProductsCount = await page.getByRole('article').count();
+
+	expect(filteredProductsCount).not.toEqual(productsCount);
+
+	// clear search
+	await searchField.fill('');
+
+	filteredProductsCount = await page.getByRole('article').count();
+
+	expect(filteredProductsCount).toEqual(productsCount);
 });
 
 test('show info text if search term matches no product name', async ({ page }) => {
